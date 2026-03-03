791accfba7670d9b686f2520e986737d13d78460
Da Chen <duh17@users.noreply.github.com>
2026-03-03 06:33:08 -0800

    feat: open chat links in-app and auto-link bare URLs

--- ios/Oppi/Core/Extensions/NotificationNames.swift
+++ ios/Oppi/Core/Extensions/NotificationNames.swift
@@ -2,4 +2,5 @@ import Foundation
 
 extension Notification.Name {
     static let inviteDeepLinkTapped = Notification.Name("\(AppIdentifiers.subsystem).inviteDeepLinkTapped")
+    static let webLinkTapped = Notification.Name("\(AppIdentifiers.subsystem).webLinkTapped")
 }
